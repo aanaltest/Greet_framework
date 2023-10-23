c276bca19e93e22d1c08c4a9c1b17206ec7d4bf7
Aanal Trivedi <aanal.trivedi@innodeed.com>
2023-10-23 15:05:51 +0530

    framework added

---
+++ Greet_A.xcframework/ios-arm64/Greet_A.framework/Headers/Greet_A.h
@@ -0,0 +1,18 @@
+//
+//  Greet_A.h
+//  Greet_A
+//
+//  Created by SAPPHIRE on 23/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Greet_A.
+FOUNDATION_EXPORT double Greet_AVersionNumber;
+
+//! Project version string for Greet_A.
+FOUNDATION_EXPORT const unsigned char Greet_AVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Greet_A/PublicHeader.h>
+
+
